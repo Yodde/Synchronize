ab89612d865ba81879741f5fddf945140e3a5f7f
Yodde <szymon.klebowski@student.put.edu.pl>
2015-06-25 12:36:46 +0200

    Wersja C#4

---
+++ Synchronize/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Synchronize {
+    class Program {
+        static void Main(string[] args) {
+
+            const int n = 1000000;
+            const int maxValue = 100;
+            var random = new Random();
+            var table = new int[n];
+            var check = new TableTasks();
+            for(int i = 0; i < n; ++i) {
+                table[i] = random.Next(maxValue);
+            }
+            Console.WriteLine("\nSum of table values: {0}",check.sumOfTableValues(n,table));
+            Console.ReadKey();
+        }
+    }
+    class TableTasks {
+        public UInt64 sumOfTableValues(int n, int[] table) {
+            UInt64 tableSum = 0;
+            var tasks = new List<Task>();
+            for(int i = 0; i < n; ++i) {
+                int tmp = i;
+                tasks.Add(Task.Run(() => {
+                    lock(this) {
+                        tableSum += (UInt64)table[tmp];
+                    }
+                }));
+            }
+            Task.WaitAll(tasks.ToArray());
+            return tableSum;
+        }
+    }
+}
